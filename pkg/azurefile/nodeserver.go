7bbaae90725d0d7d26f2c749e541df767aad259c
andyzhangx <xiazhang@microsoft.com>
2020-08-03 02:26:06 +0000

    fix: add mountOptions support on NFS

--- pkg/azurefile/nodeserver.go
+++ pkg/azurefile/nodeserver.go
@@ -168,7 +168,7 @@ func (d *Driver) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRe
 
 	var mountOptions, sensitiveMountOptions []string
 	if fsType == nfs {
-		mountOptions = []string{"vers=4,minorversion=1,sec=sys"}
+		mountOptions = util.JoinMountOptions(mountFlags, []string{"vers=4,minorversion=1,sec=sys"})
 	} else {
 		if runtime.GOOS == "windows" {
 			mountOptions = []string{fmt.Sprintf("AZURE\\%s", accountName), accountKey}
