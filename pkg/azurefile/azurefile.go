e6a52ed950d2bdd623a13f5393c3c0c78081d2b6
Ubuntu <nearora@microsoft.com>
2021-05-03 18:00:20 +0000

    feat: Enable creation of storage accounts that support large file shares

--- pkg/azurefile/azurefile.go
+++ pkg/azurefile/azurefile.go
@@ -85,6 +85,7 @@ const (
 	storageAccountField               = "storageaccount"
 	storageAccountTypeField           = "storageaccounttype"
 	skuNameField                      = "skuname"
+	enableLargeFileSharesField        = "enablelargefileshares"
 	resourceGroupField                = "resourcegroup"
 	locationField                     = "location"
 	secretNamespaceField              = "secretnamespace"
