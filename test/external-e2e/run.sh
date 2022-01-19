b3fe35f1811ea953070630405be3d7454db35e9f
andyzhangx <xiazhang@microsoft.com>
2022-01-19 09:16:40 +0000

    feat: upgrade snapshot CRDs to v1

--- test/external-e2e/run.sh
+++ test/external-e2e/run.sh
@@ -33,9 +33,8 @@ setup_e2e_binaries() {
         # enable fsGroupPolicy (only available from k8s 1.20)
         export EXTRA_HELM_OPTIONS="--set feature.enableFSGroupPolicy=true"
     fi
-    export EXTRA_HELM_OPTIONS="${EXTRA_HELM_OPTIONS} --set snapshot.apiVersion=ga"
     # test on alternative driver name
-    EXTRA_HELM_OPTIONS=$EXTRA_HELM_OPTIONS" --set driver.name=$DRIVER.csi.azure.com --set controller.name=csi-$DRIVER-controller --set linux.dsName=csi-$DRIVER-node --set windows.dsName=csi-$DRIVER-node-win"
+    export EXTRA_HELM_OPTIONS=$EXTRA_HELM_OPTIONS" --set driver.name=$DRIVER.csi.azure.com --set controller.name=csi-$DRIVER-controller --set linux.dsName=csi-$DRIVER-node --set windows.dsName=csi-$DRIVER-node-win"
     sed -i "s/file.csi.azure.com/$DRIVER.csi.azure.com/g" deploy/example/storageclass-azurefile-csi.yaml
     sed -i "s/file.csi.azure.com/$DRIVER.csi.azure.com/g" deploy/example/storageclass-azurefile-nfs.yaml
     make e2e-bootstrap
