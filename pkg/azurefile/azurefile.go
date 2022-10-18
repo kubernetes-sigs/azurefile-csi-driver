259b8c35cf47d0c377d4431583074ed6b8844317
andyzhangx <xiazhang@microsoft.com>
2022-10-18 07:17:31 +0000

    chore: disable PUBLISH_UNPUBLISH_VOLUME cap

--- pkg/azurefile/azurefile.go
+++ pkg/azurefile/azurefile.go
@@ -307,7 +307,6 @@ func (d *Driver) Run(endpoint, kubeconfig string, testBool bool) {
 	d.AddControllerServiceCapabilities(
 		[]csi.ControllerServiceCapability_RPC_Type{
 			csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
-			csi.ControllerServiceCapability_RPC_PUBLISH_UNPUBLISH_VOLUME,
 			csi.ControllerServiceCapability_RPC_CREATE_DELETE_SNAPSHOT,
 			//csi.ControllerServiceCapability_RPC_LIST_SNAPSHOTS,
 			csi.ControllerServiceCapability_RPC_EXPAND_VOLUME,
