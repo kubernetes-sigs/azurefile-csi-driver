520e02114c3d09567624dad7e94bad6a8c4038dd
andyzhangx <xiazhang@microsoft.com>
2023-01-13 01:37:45 +0000

    test: increase account limit in test

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -273,7 +273,7 @@ func checkAccountCreationLeak() {
 	framework.ExpectNoError(err, fmt.Sprintf("failed to GetAccountNumByResourceGroup(%s): %v", creds.ResourceGroup, err))
 	ginkgo.By(fmt.Sprintf("GetAccountNumByResourceGroup(%s) returns %d accounts", creds.ResourceGroup, accountNum))
 
-	accountLimitInTest := 12
+	accountLimitInTest := 13
 	framework.ExpectEqual(accountNum >= accountLimitInTest, false, fmt.Sprintf("current account num %d should not exceed %d", accountNum, accountLimitInTest))
 }
 
