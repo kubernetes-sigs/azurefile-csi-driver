38acd029aa675605df50cb4f425c82324cacd6d7
andyzhangx <xiazhang@microsoft.com>
2023-03-04 11:51:52 +0000

    chore: fix golint failure

--- pkg/os/smb/smb.go
+++ pkg/os/smb/smb.go
@@ -46,19 +46,15 @@ func IsSmbMapped(remotePath string) (bool, error) {
 // Since os.Symlink is currently being used in working code paths, no attempt is made in
 // alpha to merge the paths.
 func NewSmbLink(remotePath, localPath string) error {
-
 	if !strings.HasSuffix(remotePath, "\\") {
 		// Golang has issues resolving paths mapped to file shares if they do not end in a trailing \
 		// so add one if needed.
 		remotePath = remotePath + "\\"
 	}
 
-	cmdLine := fmt.Sprintf(`New-Item -ItemType SymbolicLink $Env:smblocalPath -Target $Env:smbremotepath`)
+	cmdLine := `New-Item -ItemType SymbolicLink $Env:smblocalPath -Target $Env:smbremotepath`
 	cmd := exec.Command("powershell", "/c", cmdLine)
-	cmd.Env = append(os.Environ(),
-		fmt.Sprintf("smbremotepath=%s", remotePath),
-		fmt.Sprintf("smblocalpath=%s", localPath),
-	)
+	cmd.Env = append(os.Environ(), "smbremotepath="+remotePath, "smblocalpath=%s"+localPath)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error linking %s to %s. output: %s, err: %v", remotePath, localPath, string(output), err)
