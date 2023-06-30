d730fef7423c002e34dc2dbd8cd48e7530daf63b
andyzhangx <xiazhang@microsoft.com>
2023-06-30 11:54:57 +0000

    fix: don't use account search cache in RandomMatchingAccount mode

--- pkg/azurefile/controllerserver.go
+++ pkg/azurefile/controllerserver.go
@@ -434,7 +434,7 @@ func (d *Driver) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest)
 			if err != nil {
 				return nil, status.Errorf(codes.Internal, err.Error())
 			}
-			if cache != nil {
+			if cache != nil && !selectRandomMatchingAccount {
 				accountName = cache.(string)
 			} else {
 				d.volLockMap.LockEntry(lockKey)
