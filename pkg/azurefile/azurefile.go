81f127ee8e3c0ff53703408dd62fd04b95775fbf
andyzhangx <xiazhang@microsoft.com>
2024-02-15 04:14:06 +0000

    chore: respect RetryAfter in throttling condition
    fix

--- pkg/azurefile/azurefile.go
+++ pkg/azurefile/azurefile.go
@@ -162,6 +162,7 @@ const (
 	// define different sleep time when hit throttling
 	accountOpThrottlingSleepSec = 16
 	fileOpThrottlingSleepSec    = 180
+	maxThrottlingSleepSec       = 1200
 
 	defaultAccountNamePrefix = "f"
 
