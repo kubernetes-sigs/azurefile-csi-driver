76997107bf4924367081095b4c2fa1b295e752fe
andyzhangx <xiazhang@microsoft.com>
2024-03-15 06:36:47 +0000

    feat: upgrade azcopy version to 10.23.0 for volume clone feature

--- test/sanity/run-test.sh
+++ test/sanity/run-test.sh
@@ -40,7 +40,7 @@ azcopyPath="/usr/local/bin/azcopy"
 if [ ! -f "$azcopyPath" ]; then
   azcopyTarFile="azcopy.tar.gz"
   echo 'Downloading azcopy...'
-  wget -O $azcopyTarFile azcopyvnext.azureedge.net/releases/release-10.22.2-20240110/azcopy_linux_amd64_10.22.2.tar.gz
+  wget -O $azcopyTarFile azcopyvnext.azureedge.net/releases/release-10.23.0-20240129/azcopy_linux_amd64_10.23.0.tar.gz
   tar -zxvf $azcopyTarFile
   mv ./azcopy*/azcopy /usr/local/bin/azcopy
   rm -rf ./$azcopyTarFile
