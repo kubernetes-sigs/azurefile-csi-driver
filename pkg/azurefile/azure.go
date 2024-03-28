14c8039a0f5c73106bb7ea4140e14bd94180db02
weizhichen <weizhichen@microsoft.com>
2024-03-28 07:01:43 +0000

    fix: workload identity did not work

--- pkg/azurefile/azure.go
+++ pkg/azurefile/azure.go
@@ -120,7 +120,18 @@ func getCloudProvider(ctx context.Context, kubeconfig, nodeID, secretName, secre
 		}
 	} else {
 		config.UserAgent = userAgent
-		if err = az.InitializeCloudFromConfig(context.TODO(), config, fromSecret, false); err != nil {
+		// these environment variables are injected by workload identity webhook
+		if tenantID := os.Getenv("AZURE_TENANT_ID"); tenantID != "" {
+			config.TenantID = tenantID
+		}
+		if clientID := os.Getenv("AZURE_CLIENT_ID"); clientID != "" {
+			config.AADClientID = clientID
+		}
+		if federatedTokenFile := os.Getenv("AZURE_FEDERATED_TOKEN_FILE"); federatedTokenFile != "" {
+			config.AADFederatedTokenFile = federatedTokenFile
+			config.UseFederatedWorkloadIdentityExtension = true
+		}
+		if err = az.InitializeCloudFromConfig(ctx, config, fromSecret, false); err != nil {
 			klog.Warningf("InitializeCloudFromConfig failed with error: %v", err)
 		}
 	}
