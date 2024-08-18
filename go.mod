e3c0e5d8db61e4e4fbd3816c4a82862ce02d3e1d
andyzhangx <xiazhang@microsoft.com>
2024-08-18 06:44:42 +0000

    test: use workload identity for e2e test

--- go.mod
+++ go.mod
@@ -19,7 +19,6 @@ require (
 	github.com/kubernetes-csi/external-snapshotter/client/v4 v4.2.0
 	github.com/onsi/gomega v1.31.1
 	github.com/pborman/uuid v1.2.0
-	github.com/pelletier/go-toml v1.9.4
 	github.com/rubiojr/go-vhd v0.0.0-20200706105327-02e210299021
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/net v0.26.0
--- test/utils/credentials/credentials_test.go
+++ test/utils/credentials/credentials_test.go
@@ -25,29 +25,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	fakeAzureCredentials = `
-	[Creds]
-	ClientID = "df7269f2-xxxx-xxxx-xxxx-0f12a7d97404"
-	ClientSecret = "8c416dc5-xxxx-xxxx-xxxx-d77069e2a255"
-	TenantID = "72f988bf-xxxx-xxxx-xxxx-2d7cd011db47"
-	SubscriptionID = "b9d2281e-xxxx-xxxx-xxxx-0d50377cdf76"
-	StorageAccountName = "TestStorageAccountName"
-	StorageAccountKey = "TestStorageAccountKey"
-	`
-)
-
 func TestCreateAzureCredentialFileOnAzureChinaCloud(t *testing.T) {
-	t.Run("WithAzureCredentials", func(t *testing.T) {
-		os.Setenv(tenantIDChinaEnvVar, "")
-		os.Setenv(subscriptionIDChinaEnvVar, "")
-		os.Setenv(aadClientIDChinaEnvVar, "")
-		os.Setenv(aadClientSecretChinaEnvVar, "")
-		os.Setenv(resourceGroupChinaEnvVar, "test-resource-group")
-		os.Setenv(locationChinaEnvVar, "test-location")
-		withAzureCredentials(t, true)
-	})
-
 	t.Run("WithEnvironmentVariables", func(t *testing.T) {
 		os.Setenv(tenantIDChinaEnvVar, "test-tenant-id")
 		os.Setenv(subscriptionIDChinaEnvVar, "test-subscription-id")
@@ -55,21 +33,12 @@ func TestCreateAzureCredentialFileOnAzureChinaCloud(t *testing.T) {
 		os.Setenv(aadClientSecretChinaEnvVar, "test-aad-client-secret")
 		os.Setenv(resourceGroupChinaEnvVar, "test-resource-group")
 		os.Setenv(locationChinaEnvVar, "test-location")
+		os.Setenv(federatedTokenFileVar, "/tmp/federated-token-file")
 		withEnvironmentVariables(t, true)
 	})
 }
 
 func TestCreateAzureCredentialFileOnAzurePublicCloud(t *testing.T) {
-	t.Run("WithAzureCredentials", func(t *testing.T) {
-		os.Setenv(tenantIDEnvVar, "")
-		os.Setenv(subscriptionIDEnvVar, "")
-		os.Setenv(aadClientIDEnvVar, "")
-		os.Setenv(aadClientSecretEnvVar, "")
-		os.Setenv(resourceGroupEnvVar, "test-resource-group")
-		os.Setenv(locationEnvVar, "test-location")
-		withAzureCredentials(t, false)
-	})
-
 	t.Run("WithEnvironmentVariables", func(t *testing.T) {
 		os.Setenv(tenantIDEnvVar, "test-tenant-id")
 		os.Setenv(subscriptionIDEnvVar, "test-subscription-id")
@@ -77,74 +46,9 @@ func TestCreateAzureCredentialFileOnAzurePublicCloud(t *testing.T) {
 		os.Setenv(aadClientSecretEnvVar, "test-aad-client-secret")
 		os.Setenv(resourceGroupEnvVar, "test-resource-group")
 		os.Setenv(locationEnvVar, "test-location")
+		os.Setenv(federatedTokenFileVar, "/tmp/federated-token-file")
 		withEnvironmentVariables(t, false)
 	})
-}
-
-func withAzureCredentials(t *testing.T, isAzureChinaCloud bool) {
-	tempFile, err := os.CreateTemp("", "azure.toml")
-	assert.NoError(t, err)
-	defer func() {
-		err := os.Remove(tempFile.Name())
-		assert.NoError(t, err)
-	}()
-
-	os.Setenv("AZURE_CREDENTIALS", tempFile.Name())
-
-	_, err = tempFile.Write([]byte(fakeAzureCredentials))
-	assert.NoError(t, err)
-
-	creds, err := CreateAzureCredentialFile(isAzureChinaCloud)
-	assert.NoError(t, err)
-	defer func() {
-		err := DeleteAzureCredentialFile()
-		assert.NoError(t, err)
-	}()
-
-	var cloud string
-	if isAzureChinaCloud {
-		cloud = AzureChinaCloud
-	} else {
-		cloud = AzurePublicCloud
-	}
-
-	assert.Equal(t, cloud, creds.Cloud)
-	assert.Equal(t, "72f988bf-xxxx-xxxx-xxxx-2d7cd011db47", creds.TenantID)
-	assert.Equal(t, "b9d2281e-xxxx-xxxx-xxxx-0d50377cdf76", creds.SubscriptionID)
-	assert.Equal(t, "df7269f2-xxxx-xxxx-xxxx-0f12a7d97404", creds.AADClientID)
-	assert.Equal(t, "8c416dc5-xxxx-xxxx-xxxx-d77069e2a255", creds.AADClientSecret)
-	assert.Equal(t, "test-resource-group", creds.ResourceGroup)
-	assert.Equal(t, "test-location", creds.Location)
-
-	azureCredentialFileContent, err := os.ReadFile(TempAzureCredentialFilePath)
-	assert.NoError(t, err)
-
-	const expectedAzureCredentialFileContent = `
-	{
-		"cloud": "{{.Cloud}}",
-		"tenantId": "72f988bf-xxxx-xxxx-xxxx-2d7cd011db47",
-		"aadClientId": "df7269f2-xxxx-xxxx-xxxx-0f12a7d97404",
-		"subscriptionId": "b9d2281e-xxxx-xxxx-xxxx-0d50377cdf76",
-		"aadClientSecret": "8c416dc5-xxxx-xxxx-xxxx-d77069e2a255",
-		"resourceGroup": "test-resource-group",
-		"location": "test-location",
-		"cloudProviderBackoff": true,
-		"cloudProviderBackoffRetries": 6,
-		"cloudProviderBackoffDuration": 5
-	}
-	`
-	tmpl := template.New("expectedAzureCredentialFileContent")
-	tmpl, err = tmpl.Parse(expectedAzureCredentialFileContent)
-	assert.NoError(t, err)
-
-	var buf bytes.Buffer
-	err = tmpl.Execute(&buf, struct {
-		Cloud string
-	}{
-		cloud,
-	})
-	assert.NoError(t, err)
-	assert.JSONEq(t, buf.String(), string(azureCredentialFileContent))
 }
 
 func withEnvironmentVariables(t *testing.T, isAzureChinaCloud bool) {
@@ -169,6 +73,7 @@ func withEnvironmentVariables(t *testing.T, isAzureChinaCloud bool) {
 	assert.Equal(t, "test-aad-client-secret", creds.AADClientSecret)
 	assert.Equal(t, "test-resource-group", creds.ResourceGroup)
 	assert.Equal(t, "test-location", creds.Location)
+	assert.Equal(t, "/tmp/federated-token-file", creds.AADFederatedTokenFile)
 
 	azureCredentialFileContent, err := os.ReadFile(TempAzureCredentialFilePath)
 	assert.NoError(t, err)
@@ -182,6 +87,7 @@ func withEnvironmentVariables(t *testing.T, isAzureChinaCloud bool) {
 		"aadClientSecret": "test-aad-client-secret",
 		"resourceGroup": "test-resource-group",
 		"location": "test-location",
+		"aadFederatedTokenFile": "/tmp/federated-token-file",
 		"cloudProviderBackoff": true,
 		"cloudProviderBackoffRetries": 6,
 		"cloudProviderBackoffDuration": 5
