b55bddf3dc22c11b6157a75ee7f31e3ffbe54423
mengze zhu <mengzezhu@microsoft.com>
2024-09-19 01:55:29 +0000

    [release-1.28] cleanup: upgrade golint version and fix golint errors

--- pkg/azurefile/azurefile.go
+++ pkg/azurefile/azurefile.go
@@ -315,7 +315,7 @@ func NewDriver(options *DriverOptions) *Driver {
 	driver.volumeLocks = newVolumeLocks()
 
 	var err error
-	getter := func(key string) (interface{}, error) { return nil, nil }
+	getter := func(_ string) (interface{}, error) { return nil, nil }
 
 	if driver.secretCacheMap, err = azcache.NewTimedCache(time.Minute, getter, false); err != nil {
 		klog.Fatalf("%v", err)
