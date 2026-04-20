b017d7ee60d8e856ca219ab96cffbb31403feab3
andyzhangx <xiazhang@microsoft.com>
2026-04-20 12:18:18 +0000

    Fix gofmt: use space-aligned var block

--- pkg/os/wmi/wmi.go
+++ pkg/os/wmi/wmi.go
@@ -191,8 +191,8 @@ func formatValue(v any) string {
 const rpcETooLate = 0x80010119
 
 var (
-	modole32			= windows.NewLazySystemDLL("ole32.dll")
-	procCoInitializeSecurity	= modole32.NewProc("CoInitializeSecurity")
+	modole32                = windows.NewLazySystemDLL("ole32.dll")
+	procCoInitializeSecurity = modole32.NewProc("CoInitializeSecurity")
 )
 
 // initializeSecurity calls CoInitializeSecurity with default settings.
