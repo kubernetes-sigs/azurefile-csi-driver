8b41c92bd86fb8c211e10f1f76e49de0106c4420
andyzhangx <xiazhang@microsoft.com>
2026-04-25 14:45:49 +0000

    test: add helpers to assign Storage File Data role to kubelet identity
    Add helper functions to azure_helpers.go:
    - GetKubeletIdentityObjectID: lists AKS clusters in a resource group
      and extracts the kubelet managed identity object ID
    - AssignRoleToIdentity: creates a role assignment on a resource group scope
    - EnsureKubeletStorageFileDataRole: convenience function that combines both
      to assign Storage File Data Privileged Contributor role to kubelet identity
    
    This enables e2e tests for mountWithManagedIdentity by programmatically
    setting up the required RBAC permissions.

--- test/utils/azure/azure_helpers.go
+++ test/utils/azure/azure_helpers.go
@@ -20,10 +20,15 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	armauthorization "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization/v2"
+	armcontainerservice "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v6"
 	resources "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	storage "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage/v2"
+	"github.com/google/uuid"
 	"sigs.k8s.io/azurefile-csi-driver/pkg/azurefile"
 	"sigs.k8s.io/cloud-provider-azure/pkg/azclient"
 	"sigs.k8s.io/cloud-provider-azure/pkg/azclient/accountclient"
@@ -32,9 +37,12 @@ import (
 )
 
 type Client struct {
+	subscriptionID   string
 	groupsClient     resourcegroupclient.Interface
 	accountsClient   accountclient.Interface
 	filesharesClient fileshareclient.Interface
+	aksClient        *armcontainerservice.ManagedClustersClient
+	roleClient       *armauthorization.RoleAssignmentsClient
 }
 
 func GetAzureClient(cloud, subscriptionID, clientID, tenantID, clientSecret, aadFederatedTokenFile string) (*Client, error) {
@@ -67,10 +75,23 @@ func GetAzureClient(cloud, subscriptionID, clientID, tenantID, clientSecret, aad
 	if err != nil {
 		return nil, err
 	}
+
+	aksClient, err := armcontainerservice.NewManagedClustersClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AKS client: %v", err)
+	}
+	roleClient, err := armauthorization.NewRoleAssignmentsClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create role assignments client: %v", err)
+	}
+
 	return &Client{
+		subscriptionID:   subscriptionID,
 		groupsClient:     factory.GetResourceGroupClient(),
 		accountsClient:   factory.GetAccountClient(),
 		filesharesClient: factory.GetFileShareClient(),
+		aksClient:        aksClient,
+		roleClient:       roleClient,
 	}, nil
 }
 
@@ -140,4 +161,63 @@ func (az *Client) GetAccountNumByResourceGroup(ctx context.Context, groupName st
 
 func stringPointer(s string) *string {
 	return &s
+}
+
+// GetKubeletIdentityObjectID retrieves the kubelet managed identity object ID from an AKS cluster.
+// It lists all AKS clusters in the given resource group and returns the kubelet identity of the first one found.
+func (az *Client) GetKubeletIdentityObjectID(ctx context.Context, resourceGroup string) (string, error) {
+	pager := az.aksClient.NewListByResourceGroupPager(resourceGroup, nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			return "", fmt.Errorf("failed to list AKS clusters in resource group %s: %v", resourceGroup, err)
+		}
+		for _, cluster := range page.Value {
+			if cluster.Properties != nil && cluster.Properties.IdentityProfile != nil {
+				if kubeletIdentity, ok := cluster.Properties.IdentityProfile["kubeletidentity"]; ok && kubeletIdentity.ObjectID != nil {
+					return *kubeletIdentity.ObjectID, nil
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("no AKS cluster with kubelet identity found in resource group %s", resourceGroup)
+}
+
+// AssignRoleToIdentity assigns an Azure role to a principal on a resource group scope.
+// roleDefinitionID should be just the GUID of the role definition.
+// Common roles:
+//   - Storage File Data Privileged Contributor: 69566ab7-960f-475b-8e7c-b3118f30c6bd
+//   - Storage File Data SMB Share Elevated Contributor: a7264617-510b-434b-a828-9731dc254ea7
+func (az *Client) AssignRoleToIdentity(ctx context.Context, resourceGroup, principalID, roleDefinitionID string) error {
+	scope := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s", az.subscriptionID, resourceGroup)
+	fullRoleDefID := fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/%s", az.subscriptionID, roleDefinitionID)
+	assignmentID := uuid.New().String()
+
+	_, err := az.roleClient.Create(ctx, scope, assignmentID, armauthorization.RoleAssignmentCreateParameters{
+		Properties: &armauthorization.RoleAssignmentProperties{
+			PrincipalID:      &principalID,
+			PrincipalType:    to.Ptr(armauthorization.PrincipalTypeServicePrincipal),
+			RoleDefinitionID: &fullRoleDefID,
+		},
+	}, nil)
+	if err != nil {
+		// Ignore "RoleAssignmentExists" conflict errors
+		if strings.Contains(err.Error(), "RoleAssignmentExists") {
+			return nil
+		}
+		return fmt.Errorf("failed to create role assignment for principal %s with role %s on scope %s: %v", principalID, roleDefinitionID, scope, err)
+	}
+	return nil
+}
+
+// EnsureKubeletStorageFileDataRole gets the kubelet identity from the AKS cluster in the given
+// resource group and assigns it the "Storage File Data Privileged Contributor" role on that resource group.
+func (az *Client) EnsureKubeletStorageFileDataRole(ctx context.Context, resourceGroup string) error {
+	principalID, err := az.GetKubeletIdentityObjectID(ctx, resourceGroup)
+	if err != nil {
+		return err
+	}
+	// Storage File Data Privileged Contributor
+	const storageFileDataPrivilegedContributorRoleID = "69566ab7-960f-475b-8e7c-b3118f30c6bd"
+	return az.AssignRoleToIdentity(ctx, resourceGroup, principalID, storageFileDataPrivilegedContributorRoleID)
 }
