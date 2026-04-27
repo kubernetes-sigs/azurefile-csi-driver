a15d11278e151a4fb35d0104764ef7a00118cb00
andyzhangx <xiazhang@microsoft.com>
2026-04-27 02:35:39 +0000

    fix: add missing fields to storage account lockKey
    The lockKey is used to cache and reuse storage accounts across
    CreateVolume requests. Several accountOptions fields that affect
    account properties were missing from the lockKey, causing volumes
    with different requirements to incorrectly share the same account:
    
    - requiresSmbOAuth: MI mount needs SmbOAuth-enabled accounts
    - isMultichannelEnabled: SMB multi-channel accounts differ
    - enableHTTPSTrafficOnly: affects account security config
    - publicNetworkAccess: affects network accessibility
    - vnetResourceGroup/vnetName/subnetName: affects private endpoint config

--- pkg/azurefile/controllerserver.go
+++ pkg/azurefile/controllerserver.go
@@ -618,10 +618,13 @@ func (d *Driver) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest)
 		if v, ok := d.volMap.Load(volName); ok {
 			accountName = v.(string)
 		} else {
-			lockKey = fmt.Sprintf("%s%s%s%s%s%s%s%s%v%v%v%v%v%v%v", sku, accountKind, resourceGroup, location, protocol, subsID, accountAccessTier, privateDNSZoneResourceGroup,
+			lockKey = fmt.Sprintf("%s%s%s%s%s%s%s%s%v%v%v%v%v%v%v%v%v%v%s%s%s%s",
+				sku, accountKind, resourceGroup, location, protocol, subsID, accountAccessTier, privateDNSZoneResourceGroup,
 				ptr.Deref(createPrivateEndpoint, false), ptr.Deref(allowBlobPublicAccess, false), ptr.Deref(requireInfraEncryption, false),
 				ptr.Deref(enableLFS, false), ptr.Deref(disableDeleteRetentionPolicy, false),
-				ptr.Deref(allowCrossTenantReplication, true), ptr.Deref(allowSharedKeyAccess, true))
+				ptr.Deref(allowCrossTenantReplication, true), ptr.Deref(allowSharedKeyAccess, true),
+				ptr.Deref(requiresSmbOAuth, false), ptr.Deref(isMultichannelEnabled, false),
+				enableHTTPSTrafficOnly, publicNetworkAccess, vnetResourceGroup, vnetName, subnetName)
 			// search in cache first
 			cache, err := d.accountSearchCache.Get(ctx, lockKey, azcache.CacheReadTypeDefault)
 			if err != nil {
