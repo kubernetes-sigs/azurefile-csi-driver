027c7de767de7eac94069a3978a185519303a3fd
andyzhangx <xiazhang@microsoft.com>
2026-08-10 01:25:46 +0000

    test: restore oauth standard smb snapshot case

--- test/e2e/dynamic_provisioning_test.go
+++ test/e2e/dynamic_provisioning_test.go
@@ -902,7 +902,7 @@ var _ = ginkgo.Describe("Dynamic Provisioning", func() {
 		test.Run(ctx, cs, ns)
 	})
 
-	ginkgo.It("should create a pod, write and read to it, take a standard smb volume snapshot, and validate whether it is ready to use [file.csi.azure.com]", func(ctx ginkgo.SpecContext) {
+	ginkgo.It("should create a pod, write and read to it, take a standard smb volume snapshot, and validate whether it is ready to use (oauth) [file.csi.azure.com]", func(ctx ginkgo.SpecContext) {
 		skipIfTestingInWindowsCluster()
 		skipIfUsingInTreeVolumePlugin()
 
@@ -925,7 +925,7 @@ var _ = ginkgo.Describe("Dynamic Provisioning", func() {
 			CSIDriver:       testDriver,
 			Pod:             pod,
 			ShouldOverwrite: false,
-			ShouldRestore:   false,
+			ShouldRestore:   true,
 			PodWithSnapshot: podWithSnapshot,
 			StorageClassParameters: map[string]string{
 				"skuName":                     "Standard_LRS",
